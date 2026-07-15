a7fdae47566a56b75821dc16c686b0567a679320
Ben Lee <ben.dl.lee@proton.me>
2026-07-14 23:39:34 -0700

    strip out junk

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import styles from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -13,111 +9,13 @@ function App() {
         <h1>NBA Dashboard</h1>
       </header>
       <section className={styles.center}>
-        <div className={styles.hero}>
-          <img src={heroImg} className={styles.base} width="170" height="179" alt="" />
-          <img src={reactLogo} className={styles.framework} alt="React logo" />
-          <img src={viteLogo} className={styles.vite} alt="Vite logo" />
-        </div>
         <div>
           <h1>Get started</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
-        <button
-          type="button"
-          className={styles.counter}
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
       </section>
-
-      <div className={styles.ticks}></div>
-
-      <section className={styles['next-steps']}>
-        <div className={styles.docs}>
-          <svg className={styles.icon} role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className={styles.logo} src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className={styles['button-icon']} src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.social}>
-          <svg className={styles.icon} role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className={styles['button-icon']}
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className={styles['button-icon']}
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className={styles['button-icon']}
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className={styles['button-icon']}
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className={styles.ticks}></div>
-      <section className={styles.spacer}></section>
     </>
   )
 }
