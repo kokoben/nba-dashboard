adecc3b42e2e522b71200dba0aa99f5666717026
Ben Lee <ben.dl.lee@proton.me>
2026-07-22 01:53:36 -0700

    add team search to dashboard

--- src/features/dashboard/views/Dashboard.tsx
+++ src/features/dashboard/views/Dashboard.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 import { TEAMS } from '@/constants';
 import { getTeams, type Team } from '@/features/dashboard/api';
 import styles from '@/features/dashboard/views/Dashboard.module.scss'
+import CustomInput from '@/components/CustomInput/CustomInput';
 
 function getIdFromDisplayName(displayName: string, apiTeams: Team[]): string {
   const targetTeam: Team | undefined = apiTeams.find((team: Team) => {
@@ -20,6 +21,17 @@ function Dashboard() {
   const [teams, setTeams] = useState<Team[] | null>(null);
   const [teamsIsLoading, setTeamsIsLoading] = useState<boolean>(true);
   const [teamsHasError, setTeamsHasError] = useState<boolean>(false);
+  const [searchTeamsInput, setSearchTeamsInput] = useState<string>('');
+
+  const filteredTeams = TEAMS.filter(team => {
+    return team.name.toLocaleLowerCase().trim()
+      .includes(searchTeamsInput.toLocaleLowerCase().trim());
+  })
+
+  const teamNoun: string = filteredTeams.length === 1 ? 'team' : 'teams';
+  const teamsFoundText: string = searchTeamsInput.trim().length
+    ? `${filteredTeams.length} ${teamNoun} found` : '';
+
 
   useEffect(() => {
     const controller = new AbortController();
@@ -51,6 +63,10 @@ function Dashboard() {
     };
   }, []);
 
+  function handleChangeInput(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearchTeamsInput(event.target.value);
+  }
+
   return (
     <>
       {teamsIsLoading ? (
@@ -60,23 +76,42 @@ function Dashboard() {
       ) : !teams || !teams.length ? (
         <div>No teams found</div>
       ) : (
-        <div className={styles['teams-grid']}>
-          {TEAMS.map((team) => {
-            const apiTeamId: string = getIdFromDisplayName(team.name, teams);
-            return (
-              <Link
-                key={apiTeamId}
-                to={`/teams/${apiTeamId}`}
-              >
-                <img
-                  className={styles['team-logo']}
-                  src={team.logoSrc}
-                  alt={team.name}
-                />
-              </Link>
-            )
-          })}
-        </div>
+        <>
+          <div className={styles['search-row']}>
+            <p
+              className={styles['teams-found']}
+              role='status'
+            >
+              {teamsFoundText}
+            </p>
+            <CustomInput
+              aria-label='Search teams'
+              type='search'
+              placeholder='Search teams'
+              onChange={handleChangeInput}
+              value={searchTeamsInput}
+            />
+          </div>
+          <div className={styles['teams-grid']}>
+            {filteredTeams.length === 0
+              ? 'No teams found'
+              : filteredTeams.map((team) => {
+              const apiTeamId: string = getIdFromDisplayName(team.name, teams);
+              return (
+                <Link
+                  key={apiTeamId}
+                  to={`/teams/${apiTeamId}`}
+                >
+                  <img
+                    className={styles['team-logo']}
+                    src={team.logoSrc}
+                    alt={team.name}
+                  />
+                </Link>
+              )
+            })}
+          </div>
+        </>
       )}
     </>
   )
