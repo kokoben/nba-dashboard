3467a1c3b9c409c5452ad4acad49d15af221aa21
Ben Lee <ben.dl.lee@proton.me>
2026-07-26 23:41:11 -0700

    add custom input storybook

---
+++ src/components/CustomInput/CustomInput.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { fn } from 'storybook/test';
+import CustomInput from '@/components/CustomInput/CustomInput';
+
+const meta: Meta<typeof CustomInput> = {
+  component: CustomInput,
+  argTypes: {
+    className: {
+      control: {
+        type: 'text',
+      },
+    },
+    placeholder: {
+      control: {
+        type: 'text',
+      },
+    }
+  },
+  args: {
+    className: 'test-class',
+    placeholder: 'Test placeholder',
+    onChange: fn(),
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    type: 'text',
+    'aria-label': 'Enter text',
+  }
+};
